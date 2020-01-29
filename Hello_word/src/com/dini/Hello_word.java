a2f416ae9c9d629affe6e0649e10ac6ac0bf5099
dineshkumar bm <dineshkumar bm@LAPTOP-99V3NSHI>
2020-01-29 11:38:29 +0530

    final

--- Hello_word/src/com/dini/Hello_word.java
+++ Hello_word/src/com/dini/Hello_word.java
@@ -2,6 +2,6 @@ package com.dini;
 
 class Hello_word {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!  hi rajanna"); 
     }
 }
